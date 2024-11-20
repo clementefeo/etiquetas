392f892173675c7020f93b8839c33ada59c8bec2
clemente <adeje2@gmail.com>
2024-11-20 23:13:21 +0000

    Add files via upload

--- etiquetas.py
+++ etiquetas.py
@@ -7,9 +7,8 @@ Librerías necesarias:
 
 Descripción:
     Este script genera un PDF con etiquetas que incluyen códigos de barras Code 128 y texto.
-    Se puede especificar el rango de etiquetas, el texto de la etiqueta y el nombre del archivo de salida.
+    Se puede especificar el rango de etiquetas, el texto de la etiqueta, el título, el prefijo y el nombre del archivo de salida.
 """
-
 
 import barcode
 from barcode.writer import ImageWriter
@@ -20,7 +19,7 @@ import os
 import shutil
 import argparse
 
-def generar_etiqueta(c, codigo, texto, x, y, ancho_etiqueta, alto_etiqueta):
+def generar_etiqueta(c, codigo, texto, titulo, x, y, ancho_etiqueta, alto_etiqueta):
     try:
         temp_dir = "temp_barcode_images"
         os.makedirs(temp_dir, exist_ok=True)
@@ -30,7 +29,7 @@ def generar_etiqueta(c, codigo, texto, x, y, ancho_etiqueta, alto_etiqueta):
         code = ean(codigo, writer=ImageWriter())
         code.save(filepath[:-4])
 
-        y_barcode = y + 5 * mm
+        y_barcode = y + 2 * mm
         c.drawImage(filepath, x, y_barcode, width=ancho_etiqueta, height=alto_etiqueta * 0.7, preserveAspectRatio=True)
 
         c.setFont("Helvetica", 8)
@@ -38,6 +37,12 @@ def generar_etiqueta(c, codigo, texto, x, y, ancho_etiqueta, alto_etiqueta):
         x_texto = x + (ancho_etiqueta - texto_ancho) / 2
         margen_inferior = 2 * mm
         c.drawString(x_texto, y + margen_inferior, texto)
+
+        c.setFont("Helvetica", 6)
+        titulo_ancho = c.stringWidth(titulo, "Helvetica", 6)
+        x_titulo = x + (ancho_etiqueta - titulo_ancho) / 2
+        margen_superior = alto_etiqueta - 4 * mm
+        c.drawString(x_titulo, y + margen_superior, titulo)
 
         margen_linea = 0 * mm
         c.setStrokeColorRGB(0.5, 0.5, 0.5)
@@ -48,25 +53,29 @@ def generar_etiqueta(c, codigo, texto, x, y, ancho_etiqueta, alto_etiqueta):
         print(f"Error generando etiqueta para {codigo}: {e}")
 
 
-def generar_etiquetas_pdf(num_etiquetas_inicio, num_etiquetas_fin, texto_etiqueta, filename="etiquetas.pdf"):
+def generar_etiquetas_pdf(num_etiquetas_inicio, num_etiquetas_fin, texto_etiqueta, titulo_etiqueta, prefijo_etiqueta, filename="etiquetas.pdf"):
     c = canvas.Canvas(filename, pagesize=A4)
 
-    ancho_etiqueta = 60 * mm
+    margen_horizontal = 10 * mm  # Margen izquierdo y derecho
+    ancho_total_disponible = A4[0] - 2 * margen_horizontal
+    etiquetas_por_fila = 3
+    ancho_etiqueta = ancho_total_disponible / etiquetas_por_fila
     alto_etiqueta = 30 * mm
-    etiquetas_por_fila = int(A4[0] // ancho_etiqueta)
-    x = 10 * mm
-    y = A4[1] - 10 * mm - alto_etiqueta
+
+    x = margen_horizontal
+    y = A4[1] - 10 * mm - alto_etiqueta  # Margen superior
 
     for i in range(num_etiquetas_inicio, num_etiquetas_fin + 1):
-        codigo = f"0RD-{i:05}"
+        codigo = f"{prefijo_etiqueta}-{i:05}"
         texto = texto_etiqueta
-        generar_etiqueta(c, codigo, texto, x, y, ancho_etiqueta, alto_etiqueta)
+        titulo = titulo_etiqueta
+        generar_etiqueta(c, codigo, texto, titulo, x, y, ancho_etiqueta, alto_etiqueta)
         x += ancho_etiqueta
 
         if (i % etiquetas_por_fila == 0):
-            x = 10 * mm
+            x = margen_horizontal
             y -= alto_etiqueta
-            if y < 10 * mm:
+            if y < 10 * mm:  # Margen inferior
                 c.showPage()
                 y = A4[1] - 10 * mm - alto_etiqueta
 
@@ -82,6 +91,8 @@ def main():
     parser = argparse.ArgumentParser(description="Generador de etiquetas con código de barras.")
     parser.add_argument("-n", "--numero", help="Rango de etiquetas (ej. 50-100 o un solo número)", required=True)
     parser.add_argument("-t", "--texto", help="Texto para la etiqueta entre comillas simples (ej: 'ORDENADOR HP')", required=True)
+    parser.add_argument("-T", "--titulo", help="Título para la etiqueta entre comillas simples (ej: 'NOMBRE EMPRESA')", required=True)
+    parser.add_argument("-p", "--prefijo", help="Prefijo para el código de barras (ej: 'ORD')", default="ORD")
     parser.add_argument("-o", "--output", help="Nombre del archivo PDF de salida (opcional)", default="etiquetas.pdf")
 
     args = parser.parse_args()
@@ -96,7 +107,7 @@ def main():
         if inicio <= 0 or fin <= 0 or inicio > fin:
              raise ValueError("Rango de etiquetas inválido.")
 
-        generar_etiquetas_pdf(inicio, fin, args.texto, args.output)
+        generar_etiquetas_pdf(inicio, fin, args.texto, args.titulo, args.prefijo, args.output)
         print(f"PDF generado con éxito: {args.output}")
 
     except ValueError as e:
@@ -105,6 +116,7 @@ def main():
     except Exception as e:
         print(f"Error inesperado: {e}")
         parser.print_help()
+
 
 if __name__ == "__main__":
     main()
